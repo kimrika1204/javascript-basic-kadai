c5c93bdde649acbb15eb925f3c1644e6d8616782
kimrika1204 <148954963+kimrika1204@users.noreply.github.com>
2023-11-08 15:36:50 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+console.log( today )
+
+var today = new Date();
+
+var year = today.getFullYear();
+var month = today.getMonth() + 1;
+var day = today.getDate();
+
+console.log( year + '年' + month + '月' + day + '日');
